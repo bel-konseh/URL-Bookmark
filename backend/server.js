9b84263b3a577cee5d2f3d3842add090f055e22b
bel-konseh <silasbel453@gmail.com>
2025-09-21 12:47:57 +0100

    Project setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { createClient } from '@supabase/supabase-js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+//Code goes here
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
