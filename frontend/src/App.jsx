fb548ab03933de56fdf176c3d70f4c58e84b4241
abongwa benedict <abongwabenedict@gmail.com>
2025-09-22 16:23:09 +0100

    finishing touches of sign/login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import { createBrowserRouter,createRoutesFromElements, Route,Routes,RouterProvider, Navigate,useLocation } from 'react-router-dom'
 import Home from './Pages/Home/Home'
 import Layout from './Pages/Layout'
+import Signup from './components/LoginSignup/SignupForm'
+import Login from './components/LoginSignup/login'
 function App() {
 
 
@@ -10,18 +12,21 @@ function App() {
   //   // return <Navigate to="/login" />
   //   window.location.href = "/login"
     
-  // }
+  // 
+  
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route pathname="/"  element={<Layout />}>
 
         <Route path="" element={<Home />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Home />} />
-        <Route path="/signup" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup />} />
       </Route>
+      
     )
   )
+
 
   return(
       <RouterProvider router={router} />
